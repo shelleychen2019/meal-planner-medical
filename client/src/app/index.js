76f4e210b8126e7040a935f615e711adf18e5dde
shelleychen2019 <58124109+shelleychen2019@users.noreply.github.com>
2020-11-11 21:16:28 -0800

    made app from function to class

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { NavBar} from '../components';
+import { NavBar } from '../components';
 import { AllMeals, MealsList, MoviesInsert, MoviesUpdate } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { getAllMeals } from '../api';
 
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
-        {/* Switch will render only 1 route, meals/list/:cuisine */}
-        {/* <Route path="/" exact component={NavBar} /> */}
-        {/* <Route path="/meals/list" exact component={Favorites} /> */}
-        <Route path="/meals/list" exact component={AllMeals} />
-        <Route path="/meals/list/:cuisine" exact component={MealsList} />
-        <Route path="/meals/create" exact component={MoviesInsert} />
-        <Route path="/meals/update/:id" exact component={MoviesUpdate} />
-      </Switch>
-    </Router>
-  )
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <Switch>
+          {/* Switch will render only 1 route, meals/list/:cuisine */}
+          {/* <Route path="/" exact component={NavBar} /> */}
+          {/* <Route path="/meals/list" exact component={Favorites} /> */}
+          <Route path="/meals/list" exact component={AllMeals} />
+          <Route path="/meals/list/:cuisine" exact component={MealsList} />
+          <Route path="/meals/create" exact component={MoviesInsert} />
+          <Route path="/meals/update/:id" exact component={MoviesUpdate} />
+        </Switch>
+      </Router>
+    )
+  }
 };
 export default App;
 // function App() {
